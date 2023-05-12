bda989c723fd7e8ae76bb0c5a15e5c39bcf117fb
BuildTools <unconfigured@null.spigotmc.org>
2023-05-11 23:12:23 -0400

    1

--- main.py
+++ main.py
@@ -116,9 +116,8 @@ def getChallengerPickem(api_key, event, steamID, authCode, user):
     for embed in pickem_embeds:
         for field in embed.fields:
             pickem_info.add_field(name=field.name, value=field.value, inline=True)
-    print(pickem_info)
-
         # Return the list of created embeds
+        print(pickem_info)
         return pickem_info
 
 # Function to retrive User Data from DB
