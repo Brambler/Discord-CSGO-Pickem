d0c377cd75da2b264790114ce175a7258b86e30a
Braham, Jordan <jordan.braham@mail.wvu.edu>
2023-05-13 12:31:58 -0400

    new command

--- main.py
+++ main.py
@@ -321,7 +321,7 @@ async def authorize(interaction: discord.Interaction):
         )
         embed.set_footer(text=f'{footerVar}')
         await client.change_presence(activity=discord.Game("Use /showpickem"))
-        print(f'Setting Pressence to "Use /showpickem"')
+        print(f'Setting Pressence to "Use /help"')
         await dm.send(embed=embed)
         await interaction.response.send_message(embed=embed, ephemeral=True)
 
@@ -365,6 +365,48 @@ async def showchallenger(interaction: discord.Interaction):
     await client.change_presence(activity=discord.Game("Use /showpickem"))
     print(f'Setting Pressence to "Use /showpickem"')
     await interaction.channel.send(embed=pickem_info_str)
+ 
+###########################
+##/showlegends COMMAND##
+###########################
+
+@client.tree.command()
+async def showlegends(interaction: discord.Interaction):
+    """Displays the user's Pick'em information."""
+    # Get the user's data from the Redis database
+    user_data = get_user_data(interaction.user.id)
+    if not user_data:
+        embed = discord.Embed(
+            title='Not Authorized!',
+            description='Sorry, looks like you haven\'t authorized your account yet!\nGo ahead and use the **/Authorize** command\nYou will get a DM from the bot with directions.',
+            color=0xff0000
+        )
+        embed.set_footer(text=f'{footerVar}')
+        await interaction.response.send_message(embed=embed, ephemeral=True)
+        return
+
+    # Call the function from the other script to get the user's Pick'em information
+    user_username = interaction.user.name
+
+    await client.change_presence(activity=discord.Game(f"Retrieving Pickem for {user_username}"))
+    print(f'Setting Pressence to "Retrieving Pickem for {user_username}"')
+    
+    pickem_info = getLegendsPickem(steam_api_key, event, user_data['steam_id'], user_data['pickem_auth_code'], user_username)
+
+    # Format the pickem_info as a string
+    pickem_info_str = pickem_info
+
+    pickemResponse_embed = discord.Embed(
+            title='Pickem Retrieved!',
+            description='*Only you can see this message*\n\nBelow is your pickem!\n**Your Pick\'em is now shown to everyone in this channel**\n\n*Thanks for using my bot <3*',
+            color=0x00ff00
+        )
+    pickemResponse_embed.set_footer(text=f'{footerVar}')
+    await interaction.response.send_message(embed=pickemResponse_embed, ephemeral=True)
+    await client.change_presence(activity=discord.Game("Use /showpickem"))
+    print(f'Setting Pressence to "Use /showpickem"')
+    await interaction.channel.send(embed=pickem_info_str)
+       
 
 
 client.run(os.getenv("discordToken"))
