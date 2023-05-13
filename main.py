24eca389833e8a42aaa295bfcb0f8f05d2e41197
Braham, Jordan <jordan.braham@mail.wvu.edu>
2023-05-13 13:28:49 -0400

    dumb bot

--- main.py
+++ main.py
@@ -1,55 +1,68 @@
 import discord
 import discord.ui
+import json
+import re
+import requests
+import asyncio
 import os
+import redis
+from redis.commands.json.path import Path
+from typing import Optional
+from discord import app_commands
 
-client = discord.Client()
+r = redis.from_url(os.environ.get("REDISCLOUD_URL"))
+steam_api_key = os.getenv("steam_api_key")
+event = os.getenv("event_id")
+version = "1.5_dev"
+footerVar = f"Brambles Pickem Bot - Version {version}"
+discordPressence = '"Use /showpickem"'
+
+MY_GUILD = discord.Object(id=os.getenv("discordGuildID"))  # replace with your guild id
+
+class MyClient(discord.Client):
+    def __init__(self, *, intents: discord.Intents):
+        super().__init__(intents=intents)
+        self.tree = app_commands.CommandTree(self)
+
+    async def setup_hook(self):
+        self.tree.copy_global_to(guild=MY_GUILD)
+        await self.tree.sync(guild=MY_GUILD)
 
 class MyView(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.select = discord.ui.Select(
-            placeholder="Choose a Flavor!",
-            min_values=1,
-            max_values=1,
-            options=[
-                discord.SelectOption(
-                    label="Vanilla",
-                    description="Pick this if you like vanilla!"
-                ),
-                discord.SelectOption(
-                    label="Chocolate",
-                    description="Pick this if you like chocolate!"
-                ),
-                discord.SelectOption(
-                    label="Strawberry",
-                    description="Pick this if you like strawberry!"
-                )
-            ]
-        )
-        self.add_item(self.select)
-
-    async def interaction_check(self, interaction: discord.Interaction):
-        # Only allow interactions from the same user who initiated the view
-        return interaction.user == self.ctx.author
-
-    async def on_timeout(self):
-        # Clean up the view after it times out
-        self.clear_items()
-
-    async def select_callback(self, select: discord.ui.Select, interaction: discord.Interaction):
+    @discord.ui.select(
+        placeholder="Choose a Flavor!",
+        min_values=1,
+        max_values=1,
+        options=[
+            discord.SelectOption(
+                label="Vanilla",
+                description="Pick this if you like vanilla!"
+            ),
+            discord.SelectOption(
+                label="Chocolate",
+                description="Pick this if you like chocolate!"
+            ),
+            discord.SelectOption(
+                label="Strawberry",
+                description="Pick this if you like strawberry!"
+            )
+        ]
+    )
+    async def select_callback(self, select, interaction):
         await interaction.response.send_message(f"Awesome! I like {select.values[0]} too!", ephemeral=True)
 
+intents = discord.Intents.default()
+client = MyClient(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f"We have logged in as {client.user}")
+    await client.change_presence(activity=discord.Game(discordPressence))
+    print('------------------------------')
+    print(f'Logged in as {client.user} (ID: {client.user.id})\nBot Version {version}\nCurrent Pressence: {discordPressence}')
+    print('------------------------------')
 
-
-@client.application_command()
-async def flavor(ctx: discord.ApplicationCommandInteraction):
-    view = MyView()
-    view.ctx = ctx
-    await ctx.send("Choose a flavor!", view=view)
-
+@client.tree.command()
+async def flavor(interaction: discord.Interaction):
+    await interaction.response.send_message("Choose a flavor!", ephemeral=True, view=MyView())
 
 client.run(os.getenv("discordToken"))
